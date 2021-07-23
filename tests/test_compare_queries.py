62bcd5f9e34c8e93e572312600f4bb1442321f95
RuslanBergenov <bergenov@ualberta.ca>
2021-07-23 17:03:06 -0600

    test: verify dynamic flattening queries - compare to hardcoded examples

---
+++ tests/test_compare_queries.py
@@ -0,0 +1,77 @@
+from tests.test_base import BaseUnitTest
+from tests.test_base import Context
+from cf.main import GaExportedNestedDataStorage
+from tests.rsc import sample_desired_queries
+
+class TestGenerateQuery(BaseUnitTest):
+
+    def test_check_sql_queries(self):
+        """compares dynamically generated queries to an expected baseline example"""
+
+        #TODO: refactor so there's less repetition, maybe with some loop and helper function
+
+        c = Context()
+        ga_source = GaExportedNestedDataStorage(gcp_project=c.env["project"],
+                                                dataset=BaseUnitTest.DATASET,
+                                                table_name=BaseUnitTest.TABLE_TYPE,
+                                                date_shard=BaseUnitTest.DATE,
+                                                )
+
+        # EVENTS
+        sample_hardcoded_events_query = sample_desired_queries.sample_events_query
+        sample_hardcoded_events_query = sample_hardcoded_events_query.upper().replace(" ", "").replace("\n",
+                                                                                                       "").replace("\t",
+                                                                                                                   "")
+
+        test_events_dynamic_query = ga_source.get_events_query()
+        test_events_dynamic_query = test_events_dynamic_query.replace(ga_source.gcp_project, "gcp-project").replace(
+            ga_source.dataset, "dataset").replace(ga_source.date_shard, "date_shard")
+        test_events_dynamic_query = test_events_dynamic_query.upper().replace(" ", "").replace("\n", "").replace("\t",
+                                                                                                                 "")
+
+        assert sample_hardcoded_events_query == test_events_dynamic_query
+
+        # EVENT_PARAMS
+        sample_hardcoded_event_params_query = sample_desired_queries.sample_event_params_query
+        sample_hardcoded_event_params_query = sample_hardcoded_event_params_query.upper().replace(" ", "").replace("\n",
+                                                                                                                   "").replace(
+            "\t", "")
+
+        test_event_params_dynamic_query = ga_source.get_event_params_query()
+        test_event_params_dynamic_query = test_event_params_dynamic_query.replace(ga_source.gcp_project,
+                                                                                  "gcp-project").replace(
+            ga_source.dataset, "dataset").replace(ga_source.date_shard, "date_shard")
+        test_event_params_dynamic_query = test_event_params_dynamic_query.upper().replace(" ", "").replace("\n",
+                                                                                                           "").replace(
+            "\t", "")
+
+        assert sample_hardcoded_event_params_query == test_event_params_dynamic_query
+
+        # ITEMS
+        sample_hardcoded_items_query = sample_desired_queries.sample_items_query
+        sample_hardcoded_items_query = sample_hardcoded_items_query.upper().replace(" ", "").replace("\n", "").replace(
+            "\t", "")
+
+        test_items_dynamic_query = ga_source.get_items_query()
+        test_items_dynamic_query = test_items_dynamic_query.replace(ga_source.gcp_project, "gcp-project").replace(
+            ga_source.dataset, "dataset").replace(ga_source.date_shard, "date_shard")
+        test_items_dynamic_query = test_items_dynamic_query.upper().replace(" ", "").replace("\n", "").replace("\t", "")
+
+        assert sample_hardcoded_items_query == test_items_dynamic_query
+
+        # USER PROPERTIES
+        sample_hardcoded_user_properties_query = sample_desired_queries.sample_user_properties_query
+        sample_hardcoded_user_properties_query = sample_hardcoded_user_properties_query.upper().replace(" ",
+                                                                                                        "").replace(
+            "\n", "").replace("\t", "")
+
+        test_user_properties_dynamic_query = ga_source.get_user_properties_query()
+        test_user_properties_dynamic_query = test_user_properties_dynamic_query.replace(ga_source.gcp_project,
+                                                                                        "gcp-project").replace(
+            ga_source.dataset, "dataset").replace(ga_source.date_shard, "date_shard")
+        test_user_properties_dynamic_query = test_user_properties_dynamic_query.upper().replace(" ", "").replace("\n",
+                                                                                                                 "").replace(
+            "\t", "")
+
+        assert sample_hardcoded_user_properties_query == test_user_properties_dynamic_query
+
--- tests/test_flatten_ga_data.py
+++ tests/test_flatten_ga_data.py
@@ -1,19 +1,20 @@
 from tests.test_base import BaseUnitTest
 from tests.test_base import Context
 from cf.main import GaExportedNestedDataStorage
+from tests.rsc import sample_desired_queries
 
 
 class TestCFFlattenMethods(BaseUnitTest):
-    # TODO: ga4 - update
+
     def test_flatten_ga_data(self):
         c = Context()
         ga_source = GaExportedNestedDataStorage(gcp_project=c.env["project"],
-                                           dataset=BaseUnitTest.DATASET,
-                                           table_name=BaseUnitTest.TABLE_TYPE,
-                                           date_shard=BaseUnitTest.DATE,
-                                           )
+                                                dataset=BaseUnitTest.DATASET,
+                                                table_name=BaseUnitTest.TABLE_TYPE,
+                                                date_shard=BaseUnitTest.DATE,
+                                                )
         ga_source.run_query_job(query=ga_source.get_event_params_query(), table_type="flat_event_params")
-        ga_source.run_query_job(query=ga_source.get_user_properties_query(),table_type="flat_user_properties")
+        ga_source.run_query_job(query=ga_source.get_user_properties_query(), table_type="flat_user_properties")
         ga_source.run_query_job(query=ga_source.get_items_query(), table_type="flat_items")
         ga_source.run_query_job(query=ga_source.get_events_query(), table_type="flat_events")
         self.assertTrue(True)
