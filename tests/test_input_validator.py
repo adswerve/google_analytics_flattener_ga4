95b97ad504e562b301a8da9ff00661c2c99cac67
RuslanBergenov <bergenov@ualberta.ca>
2021-08-16 19:10:58 -0600

    test: additional InputValidator logging unit test

--- tests/test_input_validator.py
+++ tests/test_input_validator.py
@@ -4,7 +4,9 @@ import json
 import base64
 from cf.main import InputValidator
 from testfixtures import log_capture
-import re
+from dm_helper import GaFlattenerDeploymentConfiguration
+import os
+
 
 class TestInputValidator(BaseUnitTest):
     def test_input_validator(self):
@@ -64,7 +66,41 @@ class TestInputValidator(BaseUnitTest):
         # check_present means "contains"
         # check means "equals"
 
+class TestInputValidatorConfigurationError(BaseUnitTest):
 
+    def setUp(self):
+        context = Context()
+        configuration = GaFlattenerDeploymentConfiguration(context.env)
+        #Set user environment variables
+        for key, value in configuration.user_environment_variables.items():
+            if key == "config_bucket_name":
+                os.environ[key] = "non-existing-bucket" # set incorrect bucket name
+            else:
+                os.environ[key] = value
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.normpath(
+                os.path.join(os.path.dirname(__file__), "..", "sandbox", "sa.json"))
 
-    def test_input_validator_Exception(self):
-        pass
+    @log_capture()
+    def test_input_validator_Exception(self, logcapture):
+        c = Context()
+        project_id = c.env["project"]
+        dataset_id = BaseUnitTest.DATASET
+        date_shard = BaseUnitTest.DATE
+        table_type = BaseUnitTest.TABLE_TYPE
+
+        SAMPLE_LOAD_DATA = {"protoPayload": {
+            "serviceData": {"jobCompletedEvent": {"job": {"jobConfiguration": {"load": {"destinationTable": {
+                "datasetId": dataset_id
+                , "projectId": project_id
+                , "tableId": "{table_type}_{date_shard}".format(table_type=table_type, date_shard=date_shard)
+            }}}}}}}}
+        SAMPLE_PUBSUB_MESSAGE = {'@type': 'type.googleapis.com/google.pubsub.v1.PubsubMessage', 'attributes':
+            {'origin': 'python-unit-test', 'username': 'gcp'}
+            , 'data': base64.b64encode(json.dumps(SAMPLE_LOAD_DATA).encode('utf-8'))}
+        iv = InputValidator(SAMPLE_PUBSUB_MESSAGE)
+
+        expected_log = ('root', 'CRITICAL',
+                        "flattener configuration error: 404 GET https://storage.googleapis.com/download/storage/v1/b/non-existing-bucket/o/config_datasets.json?alt=media: The specified bucket does not exist.: ('Request failed with status code', 404, 'Expected one of', <HTTPStatus.OK: 200>, <HTTPStatus.PARTIAL_CONTENT: 206>)")
+
+        logcapture.check_present(expected_log, )  # check that our expected_log message is present in InputValidator log
+
