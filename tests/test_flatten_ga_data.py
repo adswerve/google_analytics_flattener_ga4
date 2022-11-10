70774db4f4c8c071b01d0b5d0f45e0cc60727fbb
RuslanBergenov <bergenov@ualberta.ca>
2022-11-10 14:51:12 -0700

    fix: unit test auth - work in progress

--- tests/test_flatten_ga_data.py
+++ tests/test_flatten_ga_data.py
@@ -13,48 +13,49 @@ class TestCFFlattenMethods(BaseUnitTest):
                                             )
 
 
-    # client = bigquery.Client()
-    #
-    # def tbl_exists(self, client, dataset, table_name):
-    #     """
-    #      https://stackoverflow.com/questions/28731102/bigquery-check-if-table-already-exists
-    #      """
-    #     dataset = client.dataset(dataset)
-    #     table_ref = dataset.table(table_name)
-    #
-    #     try:
-    #         client.get_table(table_ref)
-    #         return True
-    #     except NotFound:
-    #         return False
+
+
+    def tbl_exists(self, dataset, table_name):
+        """
+         https://stackoverflow.com/questions/28731102/bigquery-check-if-table-already-exists
+         """
+        client = bigquery.Client(project=self.ga_source.gcp_project)
+        dataset = client.dataset(dataset)
+        table_ref = dataset.table(table_name)
+
+        try:
+            client.get_table(table_ref)
+            return True
+        except NotFound:
+            return False
 
     def test_flatten_ga_data_check_output_flat_event_params(self):
         self.ga_source.run_query_job(query=self.ga_source.get_event_params_query(),
                                     table_type="flat_event_params",
                                      wait_for_the_query_job_to_complete=True)
 
-        # assert self.tbl_exists(client=self.client, dataset= self.ga_source.dataset, table_name = f"flat_event_params_{self.ga_source.date_shard}")
+        assert self.tbl_exists(dataset= self.ga_source.dataset, table_name = f"flat_event_params_{self.ga_source.date_shard}")
 
     def test_flatten_ga_data_check_output_flat_events(self):
         self.ga_source.run_query_job(query=self.ga_source.get_events_query(),
                                     table_type="flat_events",
                                      wait_for_the_query_job_to_complete=True)
 
-        # assert self.tbl_exists(client=self.client, dataset=self.ga_source.dataset,
-        #                        table_name=f"flat_events_{self.ga_source.date_shard}")
+        assert self.tbl_exists(dataset=self.ga_source.dataset,
+                               table_name=f"flat_events_{self.ga_source.date_shard}")
     def test_flatten_ga_data_check_output_flat_items(self):
         self.ga_source.run_query_job(query=self.ga_source.get_items_query(),
                                      table_type="flat_items",
                                      wait_for_the_query_job_to_complete=True)
 
-        # assert self.tbl_exists(client=self.client, dataset=self.ga_source.dataset,
-        #                    table_name=f"flat_items_{self.ga_source.date_shard}")
+        assert self.tbl_exists(dataset=self.ga_source.dataset,
+                           table_name=f"flat_items_{self.ga_source.date_shard}")
     def test_flatten_ga_data_check_output_flat_user_properties(self):
         self.ga_source.run_query_job(query=self.ga_source.get_user_properties_query(),
                                      table_type="flat_user_properties",
                                      wait_for_the_query_job_to_complete=True)
 
-        # assert self.tbl_exists(client=self.client, dataset= self.ga_source.dataset, table_name = f"flat_user_properties_{self.ga_source.date_shard}")
+        assert self.tbl_exists(dataset= self.ga_source.dataset, table_name = f"flat_user_properties_{self.ga_source.date_shard}")
         #TODO: test intraday flattening
     def tearDown(self):
         self.delete_all_flat_tables_from_dataset()
